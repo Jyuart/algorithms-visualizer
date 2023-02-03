c556cdf8abbd7adb5756273d2c677b76e0031659
Yurii Palaida <yura.palayda@gmail.com>
2023-02-03 18:46:29 +0200

    Updates

--- _app/immutable/chunks/2-1e9d408f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-57bd081f.js";export{t as component};
---
+++ _app/immutable/chunks/2-600667fc.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0cb76b89.js";export{t as component};
